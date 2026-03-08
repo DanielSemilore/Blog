d223683b9c2764e90f86156dcc493f8515e0c093
DanielSemilore <semiloredaniel15@gmail.com>
2026-03-08 22:09:14 +0100

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import BlogCard from "./components/BlogCard";
 import { posts } from "./components/data/posts";
